843391432fac330ab56d189f016488bcdea1c9ee
Igor Rodionov <goruha@gmail.com>
2026-03-12 22:16:28 +0100

    Added context.tf to avoid warnings about missed variables

--- components/terraform/cluster/main.tf
+++ components/terraform/cluster/main.tf
@@ -4,6 +4,30 @@ variable "name" {
   default     = "cluster"
 }
 
+variable "namespace" {
+  type        = string
+  default     = null
+  description = "ID element. Usually an abbreviation of your organization name, e.g. 'eg' or 'cp', to help ensure generated IDs are globally unique"
+}
+
+variable "tenant" {
+  type        = string
+  default     = null
+  description = "ID element _(Rarely used, not included by default)_. A customer identifier, indicating who this instance of a resource is for"
+}
+
+variable "environment" {
+  type        = string
+  default     = null
+  description = "ID element. Usually used for region e.g. 'uw2', 'us-west-2', OR role 'prod', 'staging', 'dev', 'UAT'"
+}
+
+variable "stage" {
+  type        = string
+  default     = null
+  description = "ID element. Usually used to indicate role, e.g. 'prod', 'staging', 'source', 'build', 'test', 'deploy', 'release'"
+}
+
 variable "vpc_id" {
   description = "Mock input for vpc id"
   type        = string
